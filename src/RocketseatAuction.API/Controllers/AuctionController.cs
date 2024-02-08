aade305400a60a868dac7eaaf7225820379385af
Pedro Telles <pedro.htelles@hotmail.com>
2024-02-07 21:43:01 -0300

    Adicionado módulos da aula 2

--- src/RocketseatAuction.API/Controllers/AuctionController.cs
+++ src/RocketseatAuction.API/Controllers/AuctionController.cs
@@ -4,9 +4,7 @@ using RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 namespace RocketseatAuction.API.Controllers;
 
-[Route("api/[controller]")]
-[ApiController]
-public class AuctionController : ControllerBase
+public class AuctionController : RocketseatAuctionBaseController
 {
     [HttpGet]
     [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
